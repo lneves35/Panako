0792f11cef3b392f2a6059544f5bb19d05ae1f02
lneves35 <luis.s.neves@gmail.com>
2026-01-28 10:31:50 +0000

    Update PanakoStorageKV.java

--- src/main/java/be/panako/strategy/panako/storage/PanakoStorageKV.java
+++ src/main/java/be/panako/strategy/panako/storage/PanakoStorageKV.java
@@ -113,30 +113,30 @@ public class PanakoStorageKV implements PanakoStorage{
 		if(!new File(folder).exists()) {
 			throw new RuntimeException("Could not create LMDB folder: " + folder);
 		}
-		
-		// MINIMAL CHANGES APPLIED HERE:
-		// We use MDB_NORDAHEAD to prevent the kernel from pre-loading data into RAM.
-		// We use MDB_NOSUBDIR to reduce filesystem overhead for large 1TB files.
-		env =  org.lmdbjava.Env.create()
-        .setMapSize(1024l * 1024l * 1024l * 1024l)//1 TB max!
-        .setMaxDbs(2)
-        .setMaxReaders(Application.availableProcessors())
-        .open(new File(folder), 
-        	EnvFlags.MDB_NOSYNC, 
-        	EnvFlags.MDB_NOMETASYNC, 
-        	EnvFlags.MDB_NOTLS, 
-        	EnvFlags.MDB_NORDAHEAD,
-        	EnvFlags.MDB_NOSUBDIR);
+				
+		env = org.lmdbjava.Env.create()
+			.setMapSize(1024L * 1024L * 1024L * 1024L) // 1 TB
+			.setMaxDbs(2)
+			.setMaxReaders(Application.availableProcessors())
+			.open(new File(folder), 
+				EnvFlags.MDB_NOSYNC, 
+				EnvFlags.MDB_NOMETASYNC, 
+				EnvFlags.MDB_NOTLS, 
+				EnvFlags.MDB_NORDAHEAD,
+				EnvFlags.MDB_NOSUBDIR,
+				EnvFlags.MDB_WRITEMAP,
+				EnvFlags.MDB_MAPASYNC
+			);
 		
 		final String fingerprintName = "panako_fingerprints";
 		fingerprints = env.openDbi(fingerprintName, DbiFlags.MDB_CREATE, DbiFlags.MDB_INTEGERKEY, DbiFlags.MDB_DUPSORT, DbiFlags.MDB_DUPFIXED);
 		
-		final String resourceName = "panako_resource_map";		
-		resourceMap = env.openDbi(resourceName,DbiFlags.MDB_CREATE, DbiFlags.MDB_INTEGERKEY);
+		final String resourceName = "panako_resource_map";        
+		resourceMap = env.openDbi(resourceName, DbiFlags.MDB_CREATE, DbiFlags.MDB_INTEGERKEY);
 		
-		storeQueue = new HashMap<Long,List<long[]>>();
-		deleteQueue = new HashMap<Long,List<long[]>>();
-		queryQueue = new HashMap<Long,List<Long>>();
+		storeQueue = new HashMap<Long, List<long[]>>();
+		deleteQueue = new HashMap<Long, List<long[]>>();
+		queryQueue = new HashMap<Long, List<Long>>();
 	}
 
 	/**
